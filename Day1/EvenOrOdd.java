45757d3dd3659ab7393c0702529fabafb0baad0f
Datta Panchal <panchaldatta508@gmail.com>
2025-07-09 18:28:43 +0530

    EvenOdd Numbers

---
+++ Day1/EvenOrOdd.java
@@ -0,0 +1,17 @@
+package Day1;
+
+public class EvenOrOdd {
+    
+    public static void main(String[] args) {
+        int number = 10; // Example number
+
+        if (number % 2 == 0) {
+            System.out.println("The number is even.");
+        } else {
+            System.out.println("The number is odd.");
+        }
+    }
+    // Using Ternary Operator
+    // String result = (number % 2 == 0) ? "Even" : "Odd";
+    // System.out.println("The number is " + result + "."); 
+}
