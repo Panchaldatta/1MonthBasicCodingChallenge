349ddedc3d1331645aa331725aa419a802a53f48
Datta Panchal <panchaldatta508@gmail.com>
2025-07-09 18:18:03 +0530

    Day-1

---
+++ Day1/PositiveOrNegative.java
@@ -0,0 +1,21 @@
+package Day1;
+
+public class PositiveOrNegative {
+    
+public static void main(String[] args) {
+    int number = -5; // Example number
+
+    if (number > 0) {
+        System.out.println("The number is positive.");
+    } else if (number < 0) {
+        System.out.println("The number is negative.");
+    } else {
+        System.out.println("The number is zero.");
+    }
+ }
+}
+
+//Using Ternary Operator
+// if(num > 0 ? "Positive": "Negative");
+//     }
+// }
